b562f477d8230ff1367867ed391c48b1bf363b65
Colin But <colinbut@users.noreply.github.com>
2016-11-24 23:08:19 +0000

    maven generated template app

---
+++ src/main/java/com/mycompany/mongodb/spring/angularjs/bookstore/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.mongodb.spring.angularjs.bookstore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
