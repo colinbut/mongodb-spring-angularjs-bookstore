88c44d03069160ff394faf152c99ff1b2cd6682f
Colin But <colinbut@users.noreply.github.com>
2016-11-26 20:06:52 +0000

    add queries to the repository methods

--- java-spring-backend/src/main/java/com/mycompany/mongodb/spring/angularjs/bookstore/repository/BookRepository.java
+++ java-spring-backend/src/main/java/com/mycompany/mongodb/spring/angularjs/bookstore/repository/BookRepository.java
@@ -7,13 +7,21 @@ package com.mycompany.mongodb.spring.angularjs.bookstore.repository;
 
 import com.mycompany.mongodb.spring.angularjs.bookstore.model.Book;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
 public interface BookRepository extends MongoRepository<Book, String> {
 
+
     Book findByTitle(String title);
+
+    @Query("{ 'type' : ?0 }")
     List<Book> findByType(String type);
+
+    @Query("{ 'type' : {$ne : ?0} }")
+    List<Book> findByTypeNot(String type);
+
     List<Book> findByAuthor(String author);
 
 }
