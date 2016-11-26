c32118bfc85853beb6e453f916e0fcbedb44cf98
Colin But <colinbut@users.noreply.github.com>
2016-11-26 19:23:08 +0000

    renamed main application class

--- java-spring-backend/src/main/java/com/mycompany/mongodb/spring/angularjs/bookstore/Application.java
+++ java-spring-backend/src/main/java/com/mycompany/mongodb/spring/angularjs/bookstore/Application.java
@@ -3,7 +3,7 @@ package com.mycompany.mongodb.spring.angularjs.bookstore;
 /**
  * Application
  */
-public class App {
+public class Application{
 
     public static void main( String[] args ) {
         System.out.println("Application");
