e70aebfe4866f1f684b04b2911a1364f9fb71695
Colin But <colinbut@users.noreply.github.com>
2016-11-26 20:18:18 +0000

    Spring Data REST support and features

--- java-spring-backend/src/main/java/com/mycompany/mongodb/spring/angularjs/bookstore/repository/BookRepository.java
+++ java-spring-backend/src/main/java/com/mycompany/mongodb/spring/angularjs/bookstore/repository/BookRepository.java
@@ -8,20 +8,23 @@ package com.mycompany.mongodb.spring.angularjs.bookstore.repository;
 import com.mycompany.mongodb.spring.angularjs.bookstore.model.Book;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource(collectionResourceRel = "book", path = "book")
 public interface BookRepository extends MongoRepository<Book, String> {
 
 
-    Book findByTitle(String title);
+    Book findByTitle(@Param("title") String title);
 
     @Query("{ 'type' : ?0 }")
-    List<Book> findByType(String type);
+    List<Book> findByType(@Param("type") String type);
 
     @Query("{ 'type' : {$ne : ?0} }")
-    List<Book> findByTypeNot(String type);
+    List<Book> findByTypeNot(@Param("type") String type);
 
-    List<Book> findByAuthor(String author);
+    List<Book> findByAuthor(@Param("author") String author);
 
 }
