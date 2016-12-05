86b69e1becd64065003c44cea28f86542ee2db95
Colin But <colinbut@users.noreply.github.com>
2016-12-05 21:26:01 +0000

    AngularJS controller make REST service to call backend

--- angularjs-frontend/js/controller.js
+++ angularjs-frontend/js/controller.js
@@ -2,7 +2,18 @@
 
 var bookListController = angular.module("bookListControllers", []);
 
-bookListController.controller('bookListController', ['$scope',
+bookListController.controller('bookListController', ['$scope', 'BookList',
   function($scope) {
-    
+
+    $scope.bookList = [];
+
+    BookList.get({},
+      function success(response) {
+        console.log(response);
+        $scope.bookList = response;
+      },
+      function error(errorResponse) {
+        console.error(errorResponse);
+      }
+    );
   }]);
